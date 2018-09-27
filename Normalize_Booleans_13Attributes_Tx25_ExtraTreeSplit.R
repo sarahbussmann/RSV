04269b4358d770caf092ea50bb8ef31ac0ca4b36
Neeha Kotte <neeharika.kotte@wustl.edu>
2018-09-27 10:55:24 -0500

    Fixed 32 to 31

--- Normalize_Booleans_13Attributes_Tx25_ExtraTreeSplit.R
+++ Normalize_Booleans_13Attributes_Tx25_ExtraTreeSplit.R
@@ -1,5 +1,5 @@
 boolean_attributes <- read.csv(file.choose(), header = FALSE)
-boolean_attributes_yz <- boolean_attributes[, 18:32]
+boolean_attributes_yz <- boolean_attributes[, 18:31]
 boolean_attributes_x <- boolean_attributes[, 1:17]
 for (i in 1:14) { 
   boolean_attributes_yz[,i] <- (boolean_attributes_yz[,i] - min(boolean_attributes_yz[,i]))/(max(boolean_attributes_yz[,i])- min(boolean_attributes_yz[,i]))
